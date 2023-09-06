03c92310a99146e89f8bb7b093b882cd7e15680e
tonyx92 <tognolimartino@gmail.com>
2023-09-06 17:03:55 +0200

    binding elements per reattivita

---
+++ 5-bindings/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    //Data binding is keeping your application state and user interface synchronized.
+
+ 	let list = ['React', 'Vue', 'Svelte']
+    let filteredList = []
+	let searchQuery = ''
+
+	function filterList() {
+    filteredList = list.filter(item => {
+      return item
+              .toLowerCase()
+              .includes(searchQuery.toLowerCase())
+    })
+  }
+</script>
+
+<input
+	on:input={filterList}
+	bind:value={searchQuery}
+	type="text"
+/>
+
+<ul>
+{#each filteredList as item}
+  <li>{item}</li>
+{/each}
+</ul>
