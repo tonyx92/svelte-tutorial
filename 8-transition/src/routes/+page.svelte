8106136ac081568e015c9e0949cf09ef4fbfe6ec
tonyx92 <tognolimartino@gmail.com>
2023-09-06 17:44:30 +0200

    transition in svelte

---
+++ 8-transition/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { fade } from 'svelte/transition'
+
+    let showFade = false
+
+    function toggleFade() {
+        showFade = !showFade
+    }
+</script>
+
+<button on:click={toggleFade}>Wax poetic</button>
+
+{#if showFade}
+    <blockquote transition:fade={{delay: 250, duration:300}}>
+        Memories fade, but friends are forever
+    </blockquote>
+{/if}
