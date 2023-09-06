dc5b79f9b4dad27c49042ed917fd55d9dd5d9797
tonyx92 <tognolimartino@gmail.com>
2023-09-06 17:38:37 +0200

    esercizio creazione semplice slot per modificare child del file parente con semplicitá

---
+++ 7-slots/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<!-- In Svelte we can use slots to compose components meaning our components can contain other components and elements to be more reusable like regular HTML. -->
+
+<script>
+    import Button from "./component/Button.svelte";
+</script>
+
+<Button>
+    <span slot="icon">➕</span>
+    <span slot="text">Add</span>
+</Button>
+
+<Button>
+    <span slot="icon">💩</span>
+    <span slot="text">Delete</span>
+</Button>
---
+++ 7-slots/src/routes/component/Button.svelte
@@ -0,0 +1,4 @@
+<button>
+	<slot name="icon"></slot>
+	<slot name="text"></slot>
+</button>
