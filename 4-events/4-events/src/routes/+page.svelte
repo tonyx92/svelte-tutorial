96aead6db183d48a706b35f3ee8b707b07d5a24f
tonyx92 <tognolimartino@gmail.com>
2023-09-06 16:09:43 +0200

    esercizi su event in svelte e event modifier

---
+++ 4-events/4-events/src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+
+    //esercizio 1 event 
+	let mouse = { x: 0, y: 0 }
+
+	function handleMouseMove(event) {
+		mouse.x = event.clientX
+		mouse.y = event.clientY
+	}
+
+
+
+    //esercizio 2
+    function handleSubmit() {
+		console.log('Submit')
+	}
+</script>
+<!-- esercizio 1 -->
+<div on:mousemove={handleMouseMove}>
+	Esercizio 1:utilizziamo evento in svelte per indicare la posizione del cursore <br>La posizione del mouse é {mouse.x} x {mouse.y}
+</div>
+
+<p>Esercizo 2 aggiunta evento preventDefault</p>
+<form on:submit|preventDefault={handleSubmit}>
+	<input type="text" />
+	<button type="submit">Submit</button>
+</form>
+
+<style>
+	div {
+		height: 100vh;
+	}
+</style>
