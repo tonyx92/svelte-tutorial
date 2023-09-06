83341e85aa5b56758501b4404207f169faed04a7
tonyx92 <tognolimartino@gmail.com>
2023-09-06 14:34:33 +0200

    aggiunto primo esercizio bottone  3e reattivita svelte

--- 1-single-file-component/single-file-component/src/routes/+page.svelte
+++ 1-single-file-component/single-file-component/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+let count = 0;
+
+function increment(){
+    count += 1
+}
+
+</script>
+
+<p>Clicked {count} {count <= 1 ? 'time' : 'times'}</p>
+<button on:click={increment}>click</button>
